89a983416e9114775fd051ab8133a988c505909c
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2023-01-09 03:28:17 -0300

    feat: add footer and styling

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
 <?php wp_footer(); ?>
+<footer class="footer">
+<img width="52" height="61" alt="10up logo" src="<?php echo get_bloginfo(
+    "template_url"
+); ?>/assets/icons/10up_Logo.svg"/>
+</footer>
+</body>
+</html>
